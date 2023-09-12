38a4e5af603d5590f295ff9d9c76aa2081aa1206
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-11 23:54:16 -0700

    Game DTOs added automapper.

---
+++ src/GameStore.Service/Commons/Mappers/MapperProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GameStore.Domain.Entities.Games;
+using GameStore.Service.DTOs.Comments;
+using GameStore.Service.DTOs.Games;
+using GameStore.Service.DTOs.Genres;
+
+namespace GameStore.Service.Commons.Mappers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Game, GameCreationDto>().ReverseMap();
+            CreateMap<Game, GameResultDto>().ReverseMap();
+            CreateMap<Game, GameUpdateDto>().ReverseMap();
+
+            CreateMap<Genre, GenreCreationDto>().ReverseMap();
+            CreateMap<Genre, GenreResultDto>().ReverseMap();
+            CreateMap<Genre, GenreUpdateDto>().ReverseMap();
+
+            CreateMap<Comment, CommentCreationDto>().ReverseMap();
+            CreateMap<Comment, CommentResultDto>().ReverseMap();
+            CreateMap<Comment, CommentUpdateDto>().ReverseMap();
+        }
+    }
+}
