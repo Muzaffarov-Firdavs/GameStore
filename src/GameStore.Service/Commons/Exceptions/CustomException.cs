bbb14e0778866e879212249628318822b8aefe49
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-12 00:18:04 -0700

    Added Custom exception.

---
+++ src/GameStore.Service/Commons/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+﻿namespace GameStore.Service.Commons.Exceptions
+{
+    public class CustomException : Exception
+    {
+        public int Code { get; set; }
+        public CustomException(int code, string message) : base(message)
+        {
+            Code = code;
+        }
+    }
+}
