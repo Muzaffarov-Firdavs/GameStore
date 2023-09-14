8929e8c155b5b7f12a0ceac4e8a7fe7550d82a6a
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-14 01:27:35 -0700

    Implemented Users controllers.

---
+++ src/GameStore.Api/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using GameStore.Service.DTOs.Users;
+using GameStore.Service.Interfaces.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Api.Controllers
+{
+    public class UsersController : BaseController
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(UserCreationDto dto)
+            => Ok(await _userService.AddAsync(dto));
+
+        [HttpPut("id")]
+        public async Task<IActionResult> PutAsync(long id, UserUpdateDto dto)
+            => Ok(await _userService.ModifyAsync(id, dto));
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DaleteByIdAsync(long id)
+            => Ok(await _userService.RemoveByIdAsync(id));
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetByIdAsync(long id)
+            => Ok(await _userService.RetrieveByIdAsync(id));
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(string? search = null)
+            => Ok(await _userService.RetrieveAllAsync(search));
+    }
+}
---
+++ src/GameStore.Service/Interfaces/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using GameStore.Service.DTOs.Genres;
+using GameStore.Service.DTOs.Users;
+
+namespace GameStore.Service.Interfaces.Users
+{
+    public interface IUserService
+    {
+        ValueTask<UserResultDto> AddAsync(UserCreationDto dto);
+        ValueTask<UserResultDto> ModifyAsync(long id, UserUpdateDto dto);
+        ValueTask<bool> RemoveByIdAsync(long id);
+        ValueTask<UserResultDto> RetrieveByIdAsync(long id);
+        ValueTask<IEnumerable<UserResultDto>> RetrieveAllAsync(string search = null);
+    }
+}
