838d0963e0e7730df1001273d31603d4eab1d3bd
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-19 16:56:33 +0500

    Added new entity.

---
+++ src/GameStore.Domain/Entities/Files/Image.cs
@@ -0,0 +1,10 @@
+﻿using GameStore.Domain.Commons;
+
+namespace GameStore.Domain.Entities.Files
+{
+    public class Image : Auditable
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+    }
+}
