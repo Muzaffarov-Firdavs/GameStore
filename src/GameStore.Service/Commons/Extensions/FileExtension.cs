b84c52f8751d119a04a74232ea0d47f2d0dfc88a
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-19 18:01:43 +0500

    Added file extension.

---
+++ src/GameStore.Service/Commons/Extensions/FileExtension.cs
@@ -0,0 +1,24 @@
+﻿using GameStore.Service.DTOs.Files;
+using Microsoft.AspNetCore.Http;
+
+namespace GameStore.Service.Commons.Extensions
+{
+    public static class FileExtension
+    {
+        public async static Task<ImageCreationDto> ToImageAsync(this IFormFile file)
+        {
+            byte[] bytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+            return new ImageCreationDto
+            {
+                File = bytes,
+                FileName = file.FileName,
+                FileExtension = Path.GetExtension(file.FileName)
+            };
+        }
+    }
+}
