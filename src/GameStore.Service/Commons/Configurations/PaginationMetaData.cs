ecb8f82d28b877fae5aa63ca1a1655972049d2b4
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-19 15:11:46 +0500

    Added pagination configuration.

---
+++ src/GameStore.Service/Commons/Configurations/PaginationMetaData.cs
@@ -0,0 +1,18 @@
+﻿namespace GameStore.Service.Commons.Configurations
+{
+    public class PaginationMetaData
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+        public PaginationMetaData(int totalCount, PaginationParams @params)
+        {
+            TotalCount = totalCount;
+            TotalPages = (int)Math.Ceiling(totalCount / (double)@params.PageSize);
+            CurrentPage = @params.PageIndex;
+        }
+    }
+}
