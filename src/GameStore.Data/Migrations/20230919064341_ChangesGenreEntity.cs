162153a5acfdfb4a1d0c87aeac5c745b12525e1a
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-19 11:46:43 +0500

    Added new migrations

---
+++ src/GameStore.Data/Migrations/20230919064341_ChangesGenreEntity.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangesGenreEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Games_GameId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Genres_Games_GameId",
+                table: "Genres");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Genres_GameId",
+                table: "Genres");
+
+            migrationBuilder.DropColumn(
+                name: "GameId",
+                table: "Genres");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "GameId",
+                table: "Comments",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "GameGenre",
+                columns: table => new
+                {
+                    GamesId = table.Column<long>(type: "bigint", nullable: false),
+                    GenresId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameGenre", x => new { x.GamesId, x.GenresId });
+                    table.ForeignKey(
+                        name: "FK_GameGenre_Games_GamesId",
+                        column: x => x.GamesId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameGenre_Genres_GenresId",
+                        column: x => x.GenresId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameGenre_GenresId",
+                table: "GameGenre",
+                column: "GenresId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Games_GameId",
+                table: "Comments",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Games_GameId",
+                table: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "GameGenre");
+
+            migrationBuilder.AddColumn<long>(
+                name: "GameId",
+                table: "Genres",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "GameId",
+                table: "Comments",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Genres_GameId",
+                table: "Genres",
+                column: "GameId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Games_GameId",
+                table: "Comments",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Genres_Games_GameId",
+                table: "Genres",
+                column: "GameId",
+                principalTable: "Games",
+                principalColumn: "Id");
+        }
+    }
+}
