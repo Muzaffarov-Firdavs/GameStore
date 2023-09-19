33066b2e748b83ce013e22ea06d9ed1b481ee03b
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-19 15:14:42 +0500

    Added CartService.

---
+++ src/GameStore.Service/Interfaces/Orders/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using GameStore.Service.DTOs.Carts;
+
+namespace GameStore.Service.Interfaces.Orders
+{
+    public interface ICartService
+    {
+        ValueTask<CartItemResultDto> AddItemAsync(CartItemCreationDto dto);
+        ValueTask<CartItemResultDto> ModifyItemAsync(CartItemUpdateDto dto);
+        ValueTask<bool> RemoveItemAsync(long id);
+        ValueTask<CartItemResultDto> RetrieveByItemIdAsync(long id);
+        ValueTask<IEnumerable<CartItemResultDto>> RetrieveAllAsync();
+        ValueTask<CartResultDto> RetrieveByClientIdAsync();
+    }
+}
---
+++ src/GameStore.Service/Services/Orders/CartService.cs
@@ -0,0 +1,38 @@
+﻿using GameStore.Service.DTOs.Carts;
+using GameStore.Service.Interfaces.Orders;
+
+namespace GameStore.Service.Services.Orders
+{
+    public class CartService : ICartService
+    {
+        public ValueTask<CartItemResultDto> AddItemAsync(CartItemCreationDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<CartItemResultDto> ModifyItemAsync(CartItemUpdateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> RemoveItemAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IEnumerable<CartItemResultDto>> RetrieveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<CartResultDto> RetrieveByClientIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<CartItemResultDto> RetrieveByItemIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
