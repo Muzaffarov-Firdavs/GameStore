20e103e52084fa5dfe753cfe07c83240b8d0211b
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-19 17:22:43 +0500

    Added Image DTOs.

---
+++ src/GameStore.Service/DTOs/Files/ImageCreationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.Service.DTOs.Files
+{
+    public class ImageCreationDto
+    {
+        public byte[] File { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+    }
+}
