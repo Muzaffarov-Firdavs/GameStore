6f8a761adcb61a8e593936acfc3327b2107da3c4
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-20 12:01:01 +0500

    Added new migration for entity changes.

---
+++ src/GameStore.Data/Migrations/20230920063527_addedImageToGame.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedImageToGame : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ImageId",
+                table: "Games",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_ImageId",
+                table: "Games",
+                column: "ImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Images_ImageId",
+                table: "Games",
+                column: "ImageId",
+                principalTable: "Images",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Images_ImageId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_ImageId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Games");
+        }
+    }
+}
