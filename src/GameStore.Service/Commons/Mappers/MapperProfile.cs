17824cce9885a2753584320a7751bb2667d0d184
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-20 11:59:43 +0500

    Added image DTOs.

--- src/GameStore.Service/Commons/Mappers/MapperProfile.cs
+++ src/GameStore.Service/Commons/Mappers/MapperProfile.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using GameStore.Domain.Entities.Files;
 using GameStore.Domain.Entities.Games;
 using GameStore.Domain.Entities.Orders;
 using GameStore.Domain.Entities.Users;
 using GameStore.Service.DTOs.Carts;
 using GameStore.Service.DTOs.Comments;
+using GameStore.Service.DTOs.Files;
 using GameStore.Service.DTOs.Games;
 using GameStore.Service.DTOs.Genres;
 using GameStore.Service.DTOs.Users;
@@ -25,6 +27,9 @@ namespace GameStore.Service.Commons.Mappers
             CreateMap<Genre, GenreResultDto>().ReverseMap();
             CreateMap<Genre, GenreUpdateDto>().ReverseMap();
             CreateMap<Genre, GenreCreationDto>().ReverseMap();
+
+            CreateMap<Image, ImageCreationDto>().ReverseMap();
+            CreateMap<Image, ImageResultDto>().ReverseMap();
 
             CreateMap<Comment, CommentResultDto>().ReverseMap();
             CreateMap<Comment, CommentUpdateDto>().ReverseMap();
