032b39418a08caa7e2eb1db21c7eb05aa38106d9
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-20 11:02:34 +0500

    Updated web root path.

--- src/GameStore.Service/Commons/Helpers/EnvironmentHelper.cs
+++ src/GameStore.Service/Commons/Helpers/EnvironmentHelper.cs
@@ -3,7 +3,5 @@
     public class EnvironmentHelper
     {
         public static string WebRootPath { get; set; }
-        public static string RegionPath { get; set; }
-        public static string DistrictPath { get; set; }
     }
 }
