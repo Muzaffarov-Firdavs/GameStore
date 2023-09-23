7665e83a9a30f00aa384de9532bb6991146505ed
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-23 15:28:34 +0500

    Added details page for Game.

---
+++ GameStore.Web/Controllers/GamesController.cs
@@ -0,0 +1,83 @@
+﻿using GameStore.Service.Interfaces.Games;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Web.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly IGameService _gameService;
+
+        public GamesController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _gameService.RetrieveByIdAsync(id));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
