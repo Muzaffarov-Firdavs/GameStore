6f451c4a815b0d364ae50c6ca3aaf5bf77e19be2
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-25 10:04:16 +0500

    Some changes in domain.

--- src/GameStore.Domain/Entities/Games/Game.cs
+++ src/GameStore.Domain/Entities/Games/Game.cs
@@ -13,10 +13,10 @@ namespace GameStore.Domain.Entities.Games
         public virtual ICollection<Genre> Genres { get; set; }
         public virtual ICollection<Comment> Comments { get; set; }
 
-        public long UserId { get; set; }
-        public virtual User User { get; set; }
+        //public long UserId { get; set; }
+        //public virtual User User { get; set; }
 
-        public long ImageId { get; set; }
+        public long? ImageId { get; set; }
         public virtual Image Image { get; set; }
     }
 }
