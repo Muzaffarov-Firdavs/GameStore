fba82a084a795113c1ae909f4e5973ff6109cb2b
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-25 15:21:36 +0500

    Created Game view model.

---
+++ src/GameStore.Service/ViewModels/CreateGameViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Service.ViewModels
+{
+    public class CreateGameViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public List<long> GenresIds { get; set; }
+
+        [Required(ErrorMessage = "Please select an image.")]
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
