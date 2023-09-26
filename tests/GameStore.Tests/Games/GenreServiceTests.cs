1bd24171933200af74b3b7de97387d8a8f2e0a9c
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-26 17:19:53 +0500

    Added 3 unit tests.

--- tests/GameStore.Tests/Games/GenreServiceTests.cs
+++ tests/GameStore.Tests/Games/GenreServiceTests.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using Castle.Core.Resource;
 using GameStore.Data.Repositories;
 using GameStore.Data.UnitOfWorks;
 using GameStore.Domain.Entities.Games;
+using GameStore.Service.Commons.Exceptions;
 using GameStore.Service.DTOs.Genres;
 using GameStore.Service.Interfaces.Games;
 using GameStore.Service.Services.Games;
@@ -64,6 +66,87 @@ namespace GameStore.Tests.Games
             Assert.NotNull(result);
             Assert.Equal(expectedGenre.Id, result.Id);
             Assert.Equal(expectedGenre.Name, result.Name);
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldThrowAlreadyExistException()
+        {
+            // Arrange
+            var genreCreationDto = new GenreCreationDto { Name = "ExsistingGenre" };
+
+            var returnGenre = new Genre
+            {
+                Id = 1,
+                Name = "ExsistingGenre",
+            };
+
+            _repositoryMock
+                .Setup(r => r.SelectAsync(
+                    It.IsAny<Expression<Func<Genre, bool>>>(), It.IsAny<string[]>()))
+                .ReturnsAsync((Expression<Func<Genre, bool>> predicate, string[] includes) => returnGenre);
+
+            // Act
+            var exception = await Assert.ThrowsAsync<CustomException>(
+                async () => await _genreService.AddAsync(genreCreationDto));
+
+            // Assert
+            Assert.Equal(409, exception.Code);
+            Assert.Equal("Genre already exists.", exception.Message);
+        }
+
+        [Fact]
+        public async Task ModifyAsync_ShouldReturnUpdatedResult()
+        {
+            // Arrange
+            var exsistedGenre = new Genre { Id = 1, Name = "RPG", CreatedAt = DateTime.UtcNow };
+            
+            var genreUpdateDto = new GenreUpdateDto { Name = "Action" };
+
+            _repositoryMock
+                .Setup(r => r.SelectAsync(
+                    It.IsAny<Expression<Func<Genre, bool>>>(), It.IsAny<string[]>()))
+                .ReturnsAsync((Expression<Func<Genre, bool>> predicate, string[] includes) => exsistedGenre);
+
+            _mapperMock.Setup(m => m.Map(genreUpdateDto, exsistedGenre))
+                .Returns(new Genre { Name = "Action" });
+
+            _repositoryMock.Setup(r => r.UpdateAsync(It.IsAny<Genre>()))
+                .ReturnsAsync(new Genre { Id = 1, Name = "Action" });
+
+            _unitOfWorkMock.Setup(u => u.SaveAsync());
+
+            _mapperMock.Setup(m => m.Map<GenreResultDto>(It.IsAny<Genre>()))
+               .Returns(new GenreResultDto { Id = 1, Name = "Action" });
+
+            // Act
+            var result = await _genreService.ModifyAsync(1, genreUpdateDto);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(exsistedGenre.Id, result.Id);
+            Assert.Equal(genreUpdateDto.Name, result.Name);
+        }
+
+        [Fact]
+        public async Task ModifyAsync_ShouldThrowNotFoundException()
+        {
+            // Arrange
+            var exsistedGenre = new Genre { Id = 2, Name = "RPG", CreatedAt = DateTime.UtcNow };
+
+            var genreUpdateDto = new GenreUpdateDto { Name = "Action" };
+
+            _repositoryMock
+                .Setup(r => r.SelectAsync(
+                    It.IsAny<Expression<Func<Genre, bool>>>(), It.IsAny<string[]>()))
+                .ReturnsAsync((Expression<Func<Genre, bool>> predicate, string[] includes) => null);
+
+            // Act
+            var exception = await Assert.ThrowsAsync<CustomException>(
+                async () => await _genreService.ModifyAsync(1, genreUpdateDto));
+
+            // Assert
+            Assert.Equal(404, exception.Code);
+            Assert.Equal("Genre not found.", exception.Message);
         }
     }
 }
