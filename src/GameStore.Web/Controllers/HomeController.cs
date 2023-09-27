a5eae0045fa68a606763a157101d753ed91fca31
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-27 11:43:42 +0500

    Added filters by genre.

--- src/GameStore.Web/Controllers/HomeController.cs
+++ src/GameStore.Web/Controllers/HomeController.cs
@@ -9,21 +9,26 @@ namespace GameStore.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IGameService _gameService;
+        private readonly IGenreService _genreService;
 
-        public HomeController(IGameService gameService)
+        public HomeController(IGameService gameService, IGenreService genreService)
         {
             _gameService = gameService;
+            _genreService = genreService;
         }
 
-        public async Task<IActionResult> Index([MinLength(3)]string search)
+        public async Task<IActionResult> Index([MinLength(3)]string search, long genreId = 0)
         {
+            ViewBag.Genres = await _genreService.RetrieveAllAsync();
+
+            if (genreId != 0)
+            {
+                var genreGames = await _gameService.RetrieveAllByGenreAsync(genreId);
+                return View(genreGames);
+            }
+
             var games = await _gameService.RetrieveAllAsync(search);
             return View(games);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
