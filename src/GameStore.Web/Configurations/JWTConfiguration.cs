87f81e2f218c208d5b5a0c7fdc8bf9b534bca14b
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-09-29 19:07:05 +0500

    Configured JWT.

---
+++ src/GameStore.Web/Configurations/JWTConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+#pragma warning disable
+
+namespace GameStore.Web.Configurations
+{
+    public static class JwtConfiguration
+    {
+        public static void ConfigureAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            var config = configuration.GetSection("Jwt");
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = config["Issuer"],
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["SecretKey"]))
+                    };
+                });
+        }
+    }
+}
