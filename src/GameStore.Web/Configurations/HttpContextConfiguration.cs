874baa0ffbb62b1895b56dce6a37f0894f5a6925
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-02 18:09:22 +0500

    Configured HttpContextHelper to handle user info.

---
+++ src/GameStore.Web/Configurations/HttpContextConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using GameStore.Service.Commons.Helpers;
+
+namespace GameStore.Web.Configurations
+{
+    public static class HttpContextConfiguration
+    {
+        public static void InitAccessor(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            HttpContextHelper.Accessor = scope.ServiceProvider.GetRequiredService<IHttpContextAccessor>();
+        }
+    }
+}
