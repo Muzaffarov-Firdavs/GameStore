62bd4aac50c96c61389041ae7ba074c97ff73bc9
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-05 19:10:35 +0500

    Added extra feature for HttpContextHelper.

--- src/GameStore.Service/Commons/Helpers/HttpContextHelper.cs
+++ src/GameStore.Service/Commons/Helpers/HttpContextHelper.cs
@@ -12,6 +12,7 @@ namespace GameStore.Service.Commons.Helpers
         public static string UserRole => HttpContext?.User?.FindFirst("role")?.Value;
         public static string FirstName => HttpContext?.User?.FindFirst("FirstName")?.Value;
         public static string LastName => HttpContext?.User?.FindFirst("LastName")?.Value;
+        public static string FileName => HttpContext?.User?.FindFirst("FileName")?.Value;
 
         private static long _tempUserId;
     }
