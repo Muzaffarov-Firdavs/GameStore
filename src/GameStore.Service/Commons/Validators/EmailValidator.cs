e130c2c392292ace3843b6f9cc948db81755dbab
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-07 11:04:46 +0500

    Added email validator.

---
+++ src/GameStore.Service/Commons/Validators/EmailValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GameStore.Service.Commons.Validators
+{
+    public class EmailValidator 
+    {
+        public static bool IsValid(string email)
+        {
+            string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
+
+            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+        }
+    }
+}
