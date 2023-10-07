38f1b7ad0b0793d122ce42eb14ff03aa9be361f0
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-07 11:05:03 +0500

    Added email attribute.

---
+++ src/GameStore.Service/Commons/Attributes/EmailAttribute.cs
@@ -0,0 +1,21 @@
+﻿using GameStore.Service.Commons.Validators;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Service.Commons.Attributes
+{
+    public class EmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is null) return new ValidationResult("Set an email.");
+            else
+            {
+                string email = value.ToString()!;
+                var result = EmailValidator.IsValid(email);
+
+                if (!result) return new ValidationResult("This email is incorrect syntax.");
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
