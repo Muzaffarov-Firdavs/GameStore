a242fd021f0f6ba06deeeb2ebe6c7a11c38be826
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-09 18:53:25 +0500

    Implemented feature to add comment.

--- src/GameStore.Web/Controllers/GamesController.cs
+++ src/GameStore.Web/Controllers/GamesController.cs
@@ -1,6 +1,8 @@
 ﻿using GameStore.Service.Commons.Extensions;
+using GameStore.Service.DTOs.Comments;
 using GameStore.Service.DTOs.Games;
 using GameStore.Service.Interfaces.Games;
+using GameStore.Service.Services.Games;
 using GameStore.Service.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,11 +12,15 @@ namespace GameStore.Web.Controllers
     {
         private readonly IGameService _gameService;
         private readonly IGenreService _genreService;
+        private readonly ICommentService _commentService;
 
-        public GamesController(IGameService gameService, IGenreService genreService)
+        public GamesController(IGameService gameService,
+            IGenreService genreService,
+            ICommentService commentService)
         {
             _gameService = gameService;
             _genreService = genreService;
+            _commentService = commentService;
         }
 
         public async Task<IActionResult> Details(int id)
@@ -81,5 +87,21 @@ namespace GameStore.Web.Controllers
             await _gameService.RemoveByIdAsync(id);
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(int gameId, string text)
+        {
+            var comment = new CommentCreationDto
+            {
+                Text = text,
+                GameId = gameId,
+                UserId = 1
+            };
+
+            await _commentService.AddAsync(comment);
+
+            return RedirectToAction("Details", new { id = gameId });
+        }
+
     }
 }
