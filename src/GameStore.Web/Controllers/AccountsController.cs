78275a5ac02e35950e085e73b0f097ca0bd1e5b3
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-19 11:02:49 +0500

    Cleaned.

--- src/GameStore.Web/Controllers/AccountsController.cs
+++ src/GameStore.Web/Controllers/AccountsController.cs
@@ -2,7 +2,6 @@
 using GameStore.Service.DTOs.Accounts;
 using GameStore.Service.Interfaces.Accounts;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.Web.Controllers
