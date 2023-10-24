a790ef3e8244a170df3bbd2fb7312d5855f53ce8
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-24 19:47:04 +0500

    Added new UI components fro Cart.

---
+++ src/GameStore.Web/Controllers/CartsController.cs
@@ -0,0 +1,16 @@
+﻿using GameStore.Service.Interfaces.Orders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Web.Controllers
+{
+    public class CartsController : Controller
+    {
+        private readonly ICartService _cartService;
+        public CartsController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> Cart() => View();
+    }
+}
