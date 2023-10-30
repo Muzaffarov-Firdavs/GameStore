587cbe2efc84896d5cc9eb8155191a92de66061c
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-30 18:40:59 +0500

    Finished structure of Order contactinfo.

---
+++ src/GameStore.Web/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using GameStore.Service.DTOs.ContactInformations;
+using GameStore.Service.Interfaces.Orders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Web.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IOrderService _orderService;
+        public OrdersController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public ViewResult Order() => View("Order");
+
+        [HttpPost]
+        public async Task<IActionResult> Order(ContactInformationDto contactInfo)
+        {
+            if (!ModelState.IsValid)
+                return Order();
+
+            var info = await _orderService.AddContactInformationAsync(contactInfo);
+            await _orderService.ConfirmOrderAsync(info);
+            return View();
+        }
+    }
+}
+ 
