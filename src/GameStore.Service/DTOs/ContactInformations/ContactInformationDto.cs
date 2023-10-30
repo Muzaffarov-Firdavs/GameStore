dd4ab3d96e08dd4aeea887fcf5f20b05fd9541a7
Muzaffarov-Firdavs <firdavsmuzaffarov03@gmail.com>
2023-10-30 17:27:08 +0500

    Added ContactInformationDto.

---
+++ src/GameStore.Service/DTOs/ContactInformations/ContactInformationDto.cs
@@ -0,0 +1,24 @@
+﻿using GameStore.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Service.DTOs.ContactInformations
+{
+    public class ContactInformationDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public PaymentType PaymentType { get; set; }
+
+        [MaxLength(600)]
+        public string Comment { get; set; }
+    }
+}
